9e2751421358dac39636e719d19cf13e048e63d9
faukiofficial <faukiofficial@gmail.com>
2024-12-17 16:10:08 +0700

    update auth protector

--- client/src/utils/routeProtector.jsx
+++ client/src/utils/routeProtector.jsx
@@ -4,11 +4,8 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
     const { isAuthenticated, user } = useSelector((state) => state.user); 
-    
-    // console.log("ini jalan", isAuthenticated, user);
   
     if (!isAuthenticated || !user) {
-      console.log("ini jalan jugaa");
       return <Navigate to="/auth/login" replace />;
     }
     return <>{children}</>;
